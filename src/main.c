c3b8fc5eb9bd7ec7f471204ab8f8180110e75762
Alina-zh-123 <6817076@mail.ru>
2026-03-02 19:04:20 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
-#include "date.h"
-#include "person.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "date.h"
+#include "person.h"
 
 int cmp_students(const void *a, const void *b) {
-    Person *x = (Person*)a;
-    Person *y = (Person*)b;
+    const Person *x = (const Person*)a;
+    const Person *y = (const Person*)b;
     
     int fac_cmp = strcmp(x->fac, y->fac);
     if (fac_cmp != 0) return fac_cmp;
     
     if (x->group != y->group) return x->group - y->group;
     
-    int date_cmp = date_cmp(&x->date, &y->date);
-    if (date_cmp != 0) return date_cmp;
+    int d_cmp = compare_dates(&x->date, &y->date);
+    if (d_cmp != 0) return d_cmp;
     
     return strcmp(x->name, y->name);
 }
@@ -23,25 +23,17 @@ int main() {
     Person vuz[10];
     
     for (int i = 0; i < 10; i++) {
-        printf("Enter name, faculty, group and date of entering:\n");
-        scanf("%s %s %d %d %d %d", 
-              vuz[i].name, 
-              vuz[i].fac, 
-              &vuz[i].group, 
-              &vuz[i].date.day, 
-              &vuz[i].date.month, 
-              &vuz[i].date.year);
+        if (scanf("%s %s %d %d %d %d", 
+              vuz[i].name, vuz[i].fac, &vuz[i].group, 
+              &vuz[i].date.day, &vuz[i].date.month, &vuz[i].date.year) != 6) break;
     }
     
     qsort(vuz, 10, sizeof(Person), cmp_students);
     
     for (int i = 0; i < 10; i++) {
         printf("%s\t %d\t %02d.%02d.%d\t %s\n", 
-               vuz[i].fac,
-               vuz[i].group,
-               vuz[i].date.day,
-               vuz[i].date.month,
-               vuz[i].date.year,
-               vuz[i].name);
+               vuz[i].fac, vuz[i].group, vuz[i].date.day,
+               vuz[i].date.month, vuz[i].date.year, vuz[i].name);
     }
+    return 0;
 }
