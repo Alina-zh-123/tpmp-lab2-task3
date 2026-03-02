cba6757172c83a95f45d004b811cfcfa99b7215a
Alina-zh-123 <6817076@mail.ru>
2026-03-02 19:03:21 +0300

    Delete date.c

--- date.c
+++
@@ -1,7 +0,0 @@
-#include "date.h"
-
-int compare_dates(const Date *a, const Date *b) {
-    if (a->year != b->year) return a->year - b->year;
-    if (a->month != b->month) return a->month - b->month;
-    return a->day - b->day;
-}
